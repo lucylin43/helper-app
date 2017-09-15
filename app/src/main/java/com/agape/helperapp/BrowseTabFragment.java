d8e15262e58a19b8986348ddd8a7906c93afcae5
ljl7 <ljl7@sfu.ca>
2017-09-15 12:15:49 -0700

    Initial commit

---
+++ app/src/main/java/com/agape/helperapp/BrowseTabFragment.java
@@ -0,0 +1,10 @@
+package com.agape.helperapp;
+
+import android.support.v4.app.Fragment;
+
+public class BrowseTabFragment extends Fragment {
+    private static final String TAG = "SuggestedTabFragment";
+
+    // TODO: finish
+
+}
