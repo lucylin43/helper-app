c5d250421e16e69015eb36ea6c4af19370541454
ljl7 <ljl7@sfu.ca>
2017-09-22 11:27:43 -0700

    created code folder

---
+++ code/app/src/main/java/com/agape/helperapp/ItemInterface.java
@@ -0,0 +1,8 @@
+package com.agape.helperapp;
+
+/**
+ * Created by Lucy on 2017-09-15.
+ */
+
+public interface ItemInterface {
+}
---
+++ code/app/src/main/java/com/agape/helperapp/NeedsTabFragment.java
@@ -0,0 +1,10 @@
+package com.agape.helperapp;
+
+import android.support.v4.app.Fragment;
+
+public class NeedsTabFragment extends Fragment {
+    private static final String TAG = "SuggestedTabFragment";
+
+    // TODO: finish
+
+}
